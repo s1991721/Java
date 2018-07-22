63ede8159e4b05c4c1b0f85019734c329babcbc5
mr.lin <8524560>
2018-07-22 20:42:37 +0800

    Remarks

--- BinarySearch/src/solution/SqrtSolution.java
+++ BinarySearch/src/solution/SqrtSolution.java
@@ -22,19 +22,19 @@ public class SqrtSolution {
         int right = x;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;//防止越界
             if (mid == x / mid) {
                 return mid;
             }
 
-            if (mid < x / mid) {
-                left = mid + 1;
+            if (mid < x / mid) {//如果mid足够大，mid * mid则会越界，所以通过x / mid来防止越界
+                left = mid + 1;//1的特例已经过了，所以左脚要迈进而不是踩点
             } else {
                 right = mid - 1;
             }
         }
 
-        if (left > x / left) {
+        if (left > x / left) {//当x不能完全开平方时，取整数位
             return left - 1;
         } else {
             return left;
