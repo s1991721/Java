5636983f025ba642365880d527847980112910cb
s1991721 <278971585@qq.com>
2019-03-22 16:32:09 +0800

    Number of Islands

--- Queue&Stack/src/Main.java
+++ Queue&Stack/src/Main.java
@@ -1,14 +1,28 @@
-import solution.MyCircularQueue;
+import solution.NumberOFIslandsSolution;
 
 public class Main {
 
     public static void main(String[] args) {
-        MyCircularQueue queue=new MyCircularQueue(3);
-        System.out.println(queue.enQueue(1));
-        System.out.println(queue.enQueue(2));
-        System.out.println(queue.enQueue(3));
-        System.out.println(queue.enQueue(4));
-        System.out.println(queue.Rear());
+
+//        MyCircularQueue queue = new MyCircularQueue(3);
+//        System.out.println(queue.enQueue(1));
+//        System.out.println(queue.enQueue(2));
+//        System.out.println(queue.enQueue(3));
+//        System.out.println(queue.enQueue(4));
+//        System.out.println(queue.Rear());
+
+//        new char[][]{
+//                {'1', '1', '0', '0', '0'},
+//                {'1', '1', '0', '0', '0'},
+//                {'0', '0', '1', '0', '0'},
+//                {'0', '0', '0', '1', '1'},
+//        }
+        NumberOFIslandsSolution numberOFIslandsSolution = new NumberOFIslandsSolution();
+        int result = numberOFIslandsSolution.numIslands(new char[][]{{'1'}, {'1'}});
+
+        System.out.println("" + result);
+
 
     }
+
 }
---
+++ Queue&Stack/src/solution/NumberOFIslandsSolution.java
@@ -0,0 +1,37 @@
+package solution;
+
+public class NumberOFIslandsSolution {
+
+    public int numIslands(char[][] grid) {
+
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] != '0') {
+                    export(grid, i, j);
+                    count++;
+                }
+            }
+
+        }
+
+        return count;
+    }
+
+    private void export(char[][] grid, int x, int y) {
+        if (x < 0 || y < 0) {
+            return;
+        }
+
+        if (x < grid.length && y < grid[x].length && grid[x][y] == '1') {
+            grid[x][y] = '0';
+        } else {
+            return;
+        }
+
+        export(grid, x, y + 1);
+        export(grid, x, y - 1);
+        export(grid, x + 1, y);
+        export(grid, x - 1, y);
+    }
+}
