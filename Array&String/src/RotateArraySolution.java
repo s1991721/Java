bed4b1e545796851d9732903e38a0bb84123dd11
s1991721 <278971585@qq.com>
2019-05-17 15:38:15 +0800

    RotateArraySolution

---
+++ Array&String/src/RotateArraySolution.java
@@ -0,0 +1,28 @@
+/**
+ * Created by mr.lin on 2019/5/17
+ * <p>
+ * Rotate Array
+ * <p>
+ * https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1182/
+ */
+public class RotateArraySolution {
+
+    public void rotate(int[] nums, int k) {
+
+        reverse(nums, 0, nums.length);    //7654321
+        reverse(nums, 0, k - 1);        //5674321
+        reverse(nums, k, nums.length - 1);//5671234
+
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+}
